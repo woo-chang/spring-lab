aa365e4fd2fbfe520e1a3ae005f121165f22a894
choiwoochang <clllickme@naver.com>
2021-09-28 15:35:45 +0900

    VO class & Mapper Interface, XML(select, insert)

---
+++ keeper-spring-study/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,16 @@
+package org.zerock.mapper;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVo;
+
+import java.util.List;
+
+public interface BoardMapper {
+
+    // @Select("select * from tbl_board where bno > 0") xml로 대체
+    public List<BoardVo> getList();
+
+    public void insert(BoardVo board);
+
+    public void insertSelectKey(BoardVo boardVo);
+}
