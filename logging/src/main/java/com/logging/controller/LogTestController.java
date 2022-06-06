07b5fb42cb67ee9fab3d5fabadd85f2346eb8674
최우창 <clllickme@naver.com>
2022-06-06 17:48:54 +0900

    logging

---
+++ logging/src/main/java/com/logging/controller/LogTestController.java
@@ -0,0 +1,19 @@
+package com.logging.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
+public class LogTestController {
+
+//  public static void main(String[] args) {
+//    Logger log = LoggerFactory.getLogger(LogTestController.class);
+//    System.out.println("hello");
+//    log.info("hello");
+//  }
+
+  public static void main(String[] args) {
+    log.info("hello");
+  }
+}
