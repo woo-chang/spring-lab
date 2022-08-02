abdf5145f09052998f41086e06720f21e5da8b15
woo-chang <clllickme@naver.com>
2022-08-02 14:42:13 +0900

    WebSocket Example

---
+++ websocket/websocket_react/src/App.js
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+
+import SockJsClient from "react-stomp";
+
+function App() {
+  const $websocket = useRef(null);
+
+  const handleMsg = (msg) => {
+    console.log(msg);
+  };
+
+  const handleClickSendTo = () => {
+    $websocket.current.sendMessage("/vote1");
+  };
+
+  const handleClickSendTemplate = () => {
+    $websocket.current.sendMessage("/vote2");
+  };
+
+  return (
+    <div>
+      <SockJsClient
+        url="http://localhost:8080/v1/vote"
+        topics={["/topics/vote1", "/topics/vote2", "/topics/vote3"]}
+        onMessage={(msg) => {
+          console.log(msg);
+        }}
+        ref={$websocket}
+      />
+      <button onClick={handleClickSendTo}>SendTo</button>
+      <button onClick={handleClickSendTemplate}>SendTemplate</button>
+    </div>
+  );
+}
+
+export default App;
